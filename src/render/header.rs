5bd1bf13cee4ccdcb667ddd72a87a55275f15285
Khue Nguyen <Z5483Y@gmail.com>
2021-10-16 09:51:15 -0400

    feat: parse and render header

---
+++ src/render/header.rs
@@ -0,0 +1,13 @@
+use crate::parser::Header;
+use fancy_regex::Regex;
+
+pub fn render(lines: &mut Vec<String>) {
+    let header_pos = Header::find_headers(lines.to_vec());
+    let header_char = Regex::new(r"#").unwrap();
+
+    for heading in header_pos.iter() {
+        lines[heading.position] = header_char.replace_all(&lines[heading.position], "█").to_string();
+        lines[heading.position] = "[38;5;122m".to_owned() + &lines[heading.position].clone().to_owned();
+        lines[heading.position].push_str("[0m");
+    }
+}

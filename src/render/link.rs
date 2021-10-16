0214de8af921bb008448f50d6fdb2a4068a986c7
Khue Nguyen <Z5483Y@gmail.com>
2021-10-16 15:03:50 -0400

    fix(render): properly parse link

--- src/render/link.rs
+++ src/render/link.rs
@@ -1,16 +1,11 @@
 use regex::Regex;
 
 pub fn render(lines: &mut Vec<String>) {
-    let link_re = Regex::new(r"!?\[(?P<name>.+)\]\((?P<link>.+)\)").unwrap();
+    let link_re = Regex::new(r"!?\[(?P<name>[><?@+'`~^%&\*\{\}.!#|\\\$';,:;=/\(\),\-\w\s+]+)\]\((?P<link>[><?@+'`~^%&\*\[\]\{\}.!#|\\\$';,:;=/,\-\w\s+]+)\)").unwrap();
 
     for line in lines.iter_mut() {
-        *line = line
-            .split(" ")
-            .map(|l| {
-                let mut line = link_re.replace_all(l, "[4m[38;5;33m$name[0m").to_string();
-                line.push_str(" ");
-                line
-            })
-            .collect();
+        *line = link_re
+            .replace_all(line, "[4m[38;5;33m$name[0m")
+            .to_string();
     }
 }
--- src/render/render.rs
+++ src/render/render.rs
@@ -1,7 +1,7 @@
 pub fn render(lines: &mut Vec<String>) {
     super::codeblock::render(lines);
-    super::modifier::render(lines);
     super::blockquote::render(lines);
-    super::header::render(lines);
     super::link::render(lines);
+    super::header::render(lines);
+    super::modifier::render(lines);
 }

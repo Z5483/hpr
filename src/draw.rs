b17fc895b08bbd27cd78ac4c6918af0862a700b5
Khue Nguyen <Z5483Y@gmail.com>
2021-10-16 15:27:54 -0400

    feat(draw): add padding

--- src/draw.rs
+++ src/draw.rs
@@ -1,18 +1,59 @@
 use tui::backend::Backend;
-use tui::layout::{Constraint, Direction, Layout};
+use tui::layout::{Constraint, Direction, Layout, Rect};
 use tui::style::Style;
 use tui::text::{Span, Spans};
-use tui::widgets::Paragraph;
+use tui::widgets::{Paragraph, Wrap};
 use tui::Terminal;
 
 use ansi4tui::bytes_to_text;
 
 use crate::app::App;
 
+#[allow(dead_code)]
+pub enum PaddingDirection {
+    Top,
+    Bottom,
+    Left,
+    Right,
+    All,
+}
+
+pub fn add_padding(mut rect: Rect, n: u16, direction: PaddingDirection) -> Rect {
+    match direction {
+        PaddingDirection::Top => {
+            rect.y += n;
+            rect.height = rect.height.saturating_sub(n);
+            rect
+        }
+        PaddingDirection::Bottom => {
+            rect.height = rect.height.saturating_sub(n);
+            rect
+        }
+        PaddingDirection::Left => {
+            rect.x += n;
+            rect.width = rect.width.saturating_sub(n);
+            rect
+        }
+        PaddingDirection::Right => {
+            rect.width = rect.width.saturating_sub(n);
+            rect
+        }
+        PaddingDirection::All => {
+            rect.y += n;
+            rect.height = rect.height.saturating_sub(n * 2);
+
+            rect.x += n;
+            rect.width = rect.width.saturating_sub(n * 2);
+
+            rect
+        }
+    }
+}
+
 pub fn draw<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) {
     terminal
         .draw(|frame| {
-            let layout = Layout::default()
+            let mut layout = Layout::default()
                 .constraints(
                     [
                         Constraint::Length(1),
@@ -23,10 +64,15 @@ pub fn draw<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) {
                 )
                 .split(frame.size());
 
+            layout[1] = add_padding(layout[1], 10, PaddingDirection::Left);
+            layout[1] = add_padding(layout[1], 10, PaddingDirection::Right);
+            layout[1] = add_padding(layout[1], 2, PaddingDirection::Top);
+
             let text = bytes_to_text(app.contents[app.current_page - 1].clone());
-            let content = Paragraph::new(text);
+            let content = Paragraph::new(text).wrap(Wrap { trim: false });
 
             let line_count: u16 = app.num_of_line[app.current_page - 1] as u16;
+
             if layout[1].height < line_count {
                 app.scrolloff_limit = line_count - layout[1].height;
                 app.can_scroll = true;

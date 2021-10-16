e2ae2dc89de5116f6858bf536154932e3a746db6
Khue Nguyen <Z5483Y@gmail.com>
2021-10-16 13:26:28 -0400

    feat(render): unify render functions

--- src/render/render.rs
+++ src/render/render.rs
@@ -1,3 +1,7 @@
 pub fn render(lines: &mut Vec<String>) {
+    super::header::render(lines);
     super::codeblock::render(lines);
+    super::modifier::render(lines);
+    super::blockquote::render(lines);
+    super::link::render(lines);
 }

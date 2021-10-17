90348e4db0cc77619297b1399e9b74a8082121c3
Khue Nguyen <Z5483Y@gmail.com>
2021-10-17 16:02:07 -0400

    fix(parser): handle codeblock fence with no token

--- src/parser/codeblock.rs
+++ src/parser/codeblock.rs
@@ -8,22 +8,38 @@ pub struct CodeBlock {
 
 impl CodeBlock {
     pub fn find_blocks(lines: Vec<String>) -> Vec<CodeBlock> {
-        let cb_start_re = Regex::new(r"^```[\s+]?\w+").unwrap();
-        let cb_end_re = Regex::new(r"^```").unwrap();
+        let cb_re = Regex::new(r"^```[(\s+)?(\w+)]?").unwrap();
+
+        let mut cb_total: Vec<usize> = Vec::new();
 
         let mut cb_start: Vec<usize> = Vec::new();
         let mut cb_end: Vec<usize> = Vec::new();
+
         let mut header: Vec<String> = Vec::new();
 
         for (index, line) in lines.iter().enumerate() {
-            if cb_start_re.is_match(line) {
-                cb_start.push(index);
+            if cb_re.is_match(line) {
+                cb_total.push(index);
+            }
+        }
 
-                let mut token = line.clone();
-                token.retain(|c| !c.is_whitespace());
-                header.push(token.replace("`", "").to_string());
-            } else if cb_end_re.is_match(line) {
-                cb_end.push(index);
+        for (index, pos) in cb_total.iter().enumerate() {
+            if index % 2 == 0 {
+                cb_start.push(*pos);
+            } else if index % 2 == 1 {
+                cb_end.push(*pos);
+            }
+        }
+
+        for (index, line) in lines.iter().enumerate() {
+            if cb_start.iter().any(|&i| i == index) {
+                if line.chars().into_iter().any(char::is_alphanumeric) {
+                    let mut token = line.clone();
+                    token.retain(|c| !c.is_whitespace());
+                    header.push(token.replace("`", "").to_string());
+                } else {
+                    header.push(" ".to_string());
+                }
             }
         }
 

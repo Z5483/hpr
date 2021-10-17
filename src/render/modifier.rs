6aa748e0c6d65316d8c818724feca91be17ab4c4
Khue Nguyen <Z5483Y@gmail.com>
2021-10-17 17:00:44 -0400

    fix(render): handle alternate case

--- src/render/modifier.rs
+++ src/render/modifier.rs
@@ -1,39 +1,40 @@
 use fancy_regex::Regex;
 
 fn render_italic(lines: &mut Vec<String>) {
-    let link_re =
-        Regex::new(r"\*(?P<text>[><?@+'`~^%&\[\]\{\}.!#|\\\$';,:;=/\(\),\-\w\s+]+)\*").unwrap();
+    let regex = Regex::new(r"\*(?P<text>(?!\*).+)\*").unwrap();
+    let alt_regex = Regex::new(r"\_(?P<text>(?!\_).+)\_").unwrap();
 
     for line in lines.iter_mut() {
-        *line = link_re.replace_all(line, "[3m$text[0m").to_string();
+        *line = regex.replace_all(line, "[3m$text[0m").to_string();
+        *line = alt_regex.replace_all(line, "[3m$text[0m").to_string();
     }
 }
 
 fn render_bold(lines: &mut Vec<String>) {
-    let link_re =
-        Regex::new(r"\*\*(?P<text>[><?@+'`~^%&\[\]\{\}.!#|\\\$';,:;=/\(\),\-\w\s+]+)\*\*").unwrap();
+    let regex = Regex::new(r"\*\*(?P<text>(?!\*).+)\*\*").unwrap();
+    let alt_regex = Regex::new(r"\_\_(?P<text>(?!\_).+)\_\_").unwrap();
 
     for line in lines.iter_mut() {
-        *line = link_re.replace_all(line, "[1m$text[0m").to_string();
+        *line = regex.replace_all(line, "[1m$text[0m").to_string();
+        *line = alt_regex.replace_all(line, "[1m$text[0m").to_string();
     }
 }
 
 fn render_bold_italic(lines: &mut Vec<String>) {
-    let link_re =
-        Regex::new(r"\*\*\*(?P<text>[><?@+'`~^%&\[\]\{\}.!#|\\\$';,:;=/\(\),\-\w\s+]+)\*\*\*")
-            .unwrap();
+    let regex = Regex::new(r"\*\*\*(?P<text>(?!\*).+)\*\*\*").unwrap();
+    let alt_regex = Regex::new(r"\_\_\_(?P<text>(?!\_).+)\_\_\_").unwrap();
 
     for line in lines.iter_mut() {
-        *line = link_re.replace_all(line, "[3m[1m$text[0m").to_string();
+        *line = regex.replace_all(line, "[3m[1m$text[0m").to_string();
+        *line = alt_regex.replace_all(line, "[3m[1m$text[0m").to_string();
     }
 }
 
-fn render_tick(lines: &mut Vec<String>) {
-    let link_re =
-        Regex::new(r"``?(?P<text>[><?@+'~^%&\*\{\}\[\].!#|\\\$';,:;=/\(\),\-\w\s+]+)``?").unwrap();
+fn render_inline(lines: &mut Vec<String>) {
+    let regex = Regex::new(r"`(?P<text>(?!`).+)`").unwrap();
 
     for line in lines.iter_mut() {
-        *line = link_re
+        *line = regex
             .replace_all(line, "[48;5;235m[38;5;220m$text[0m")
             .to_string();
     }
@@ -43,5 +44,5 @@ pub fn render(lines: &mut Vec<String>) {
     render_bold_italic(lines);
     render_bold(lines);
     render_italic(lines);
-    render_tick(lines);
+    render_inline(lines);
 }

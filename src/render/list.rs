d2e0663e4ff03c7658d4e20020e747af04281b47
Khue Nguyen <Z5483Y@gmail.com>
2021-10-21 14:34:47 -0400

    feat(render): list

--- src/render/list.rs
+++ src/render/list.rs
@@ -4,6 +4,30 @@ pub fn render(lines: &mut Vec<String>) {
     let regex = Regex::new(r"^(?P<space>\s+)?(?P<char>[\+\-\*])\s(?P<line>.+)").unwrap();
 
     for line in lines.iter_mut() {
-        *line = regex.replace_all(line, "$space• $line").to_string();
+        if regex.is_match(line).unwrap() {
+            let indent: usize = {
+                let ch = line.replace(" ", "").chars().nth(0).unwrap();
+                match ch {
+                    '+' => line.split('+').nth(0).unwrap().len(),
+                    '-' => line.split('-').nth(0).unwrap().len(),
+                    '*' => line.split('*').nth(0).unwrap().len(),
+                    _ => 0,
+                }
+            };
+
+            let indent_char: String = match indent / 2 {
+                0 => String::from("•"),
+                1 => String::from("○"),
+                2 => String::from("■"),
+                3 => String::from("□"),
+                4 => String::from("◙"),
+                5 => String::from("◘"),
+                _ => String::from("□"),
+            };
+
+            *line = regex
+                .replace_all(line, "$space".to_owned() + &indent_char + " $line")
+                .to_string();
+        }
     }
 }
